2ef7a3bf2bff58aa7699a5a14d8ffa7ab56bb201
Katy-May10 <jinyefeng1994@gmail.com>
2024-10-25 20:36:13 +0900

    商品出品機能 # Active Hash 発送までの日数の設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
   belongs_to :status
   belongs_to :shippingfee
   belongs_to :prefecture
+  belongs_to :shippingDay
 
   validates :name,            presence: true
   validates :explanation,     presence: true
