74d5b3324ceeb6f7c77a10c194cc8fb81083086d
Katy-May10 <jinyefeng1994@gmail.com>
2024-10-25 16:47:11 +0900

    商品出品機能 # バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :name,            presence: true
+  validates :explanation,     presence: true
+  validates :category_id,     presence: true
+  validates :status_id,       presence: true
+  validates :shipping_fee_id, presence: true
+  validates :prefecture_id,   presence: true
+  validates :shipping_day_id, presence: true
+  validates :price,           presence: true
 end
