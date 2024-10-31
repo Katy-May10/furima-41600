27c7b70f7bb751ae612c5dbf10710c093356b257
Katy-May10 <jinyefeng1994@gmail.com>
2024-10-31 22:41:49 +0900

    商品購入機能 # フォームの情報を保存する

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,5 +5,20 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name,
+                                          :phone).merge(order_id: current_user.id)
   end
 end
