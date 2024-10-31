892bf1ab240a36006dcb47731603d68073698b9e
Katy-May10 <jinyefeng1994@gmail.com>
2024-10-31 18:26:24 +0900

    商品購入機能 # Orderモデルの修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,10 +1,10 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user, :item, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone, :order
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone, :order_id
 
   with_options presence: true do
-    validates :user
-    validates :item
+    validates :user_id
+    validates :item_id
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :city
     validates :house_number
@@ -14,8 +14,8 @@ class OrderAddress
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
 
   def save
-    order = Order.create(user: user, item: item)
+    order = Order.create(user_id: user, item_id: item)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number,
-                   building_name: building_name, phone: phone, order: order)
+                   building_name: building_name, phone: phone, order_id: order)
   end
 end
