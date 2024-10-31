b82764e0537fd0c27e6d47659bca5c0b95582785
Katy-May10 <jinyefeng1994@gmail.com>
2024-10-31 22:41:02 +0900

    商品購入機能 # Orderモデルバリデーションの変更

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,16 +2,19 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone, :order_id
 
+  validates :postal_code, presence: true,
+                          format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
+  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+
   with_options presence: true do
-    validates :user_id
-    validates :item_id
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :city
     validates :house_number
-    validates :phone
   end
 
-  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+  VALID_PHONE_REGEX = /\A\d{2,4}-\d{2,4}-\d{3,4}\z/
+
+  validates :phone, presence: true, format: { with: VALID_PHONE_REGEX, message: 'is too short' }
+  validates :phone, numericality: { only_integer: true, message: 'is invalid. Input only number' }
 
   def save
     order = Order.create(user_id: user, item_id: item)
