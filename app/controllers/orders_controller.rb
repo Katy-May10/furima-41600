26d97b9e33b2c827d31a27ba64d605752c0017a9
Katy-May10 <jinyefeng1994@gmail.com>
2024-11-01 16:53:07 +0900

    商品購入機能 # ログインしていないユーザー・自身の商品は購入できない機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,6 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :go_to_index, only: [:index, :create]
   def index
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
@@ -20,5 +22,9 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name,
                                           :phone).merge(order_id: current_user.id)
+  end
+
+  def go_to_index
+    redirect_to root_path if @item == current_user.id
   end
 end
