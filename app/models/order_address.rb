88ad97381532926e7313279bf6e9f001d50ff842
Katy-May10 <jinyefeng1994@gmail.com>
2024-11-04 16:00:27 +0900

    商品購入機能 # モデルの修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,14 +1,13 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone, :order_id
-
-  validates :postal_code, presence: true,
-                          format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
-  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone, :order_id, :token
 
   with_options presence: true do
     validates :user_id
     validates :item_id
+    validates :token
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :city
     validates :house_number
   end
@@ -19,8 +18,8 @@ class OrderAddress
   validates :phone, numericality: { only_integer: true, message: 'is invalid. Input only number' }
 
   def save
-    order = Order.create(user_id: user, item_id: item)
+    order = Order.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number,
-                   building_name: building_name, phone: phone, order_id: order)
+                   building_name: building_name, phone: phone, order_id: order.id)
   end
 end
